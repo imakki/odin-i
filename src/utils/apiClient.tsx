2e514624e13937c578d7382febd07cb829830c2c
shravanchaudhary <shravanchaudhary07@gmail.com>
2024-04-02 00:26:29 +0530

    add staging backend

--- src/utils/apiClient.tsx
+++ src/utils/apiClient.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "http://cither.staging.chatginie.com";
 // get token from local storage
 const accessToken = localStorage.getItem(
   "CognitoIdentityServiceProvider.2gh350bk1thbt7j6etfuodvli2.3a63fc8e-115a-4027-a765-8e3b430ca812.idToken"
